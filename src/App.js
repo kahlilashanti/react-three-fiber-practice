c3e4441e4f87048116c0bdb0a25a24fd04480542
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-09-17 10:49:57 -0700

    added bounding boxes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Bulb from './components/Bulb';
 import ColorPicker from './components/ColorPicker';
 import Dragable from './components/Dragable';
 import Model from './components/Model';
+import BoundingBox from './components/BoundingBox';
 
 function App() {
   //CREATED SCENE USING REACT THREE FIBER
@@ -40,18 +41,32 @@ function App() {
         <Physics>
           <Suspense fallback={null}>
             <Dragable transformGroup>
-              <Model
-                path='/tesla_model_3/scene.gltf'
-                scale={new Array(3).fill(0.01)}
-                position={[4, 0.6, 0]}
-              />
+              <BoundingBox
+                visible
+                position={[4, 4, 0]}
+                dims={[3, 2, 6]}
+                offset={[0, -0.4, 0.8]}
+              >
+                <Model
+                  path='/tesla_model_3/scene.gltf'
+                  scale={new Array(3).fill(0.01)}
+                // position={[4, 0.6, 0]}
+                />
+              </BoundingBox>
             </Dragable>
             <Dragable transformGroup>
-              <Model
-                path='/tesla_model_s/scene.gltf'
-                scale={new Array(3).fill(0.013)}
-                position={[-4, 0.2, 0]}
-              />
+              <BoundingBox
+                visible
+                position={[-4, 4, 0]}
+                dims={[3, 2, 7]}
+                offset={[0, -0.8, 0.2]}
+              >
+                <Model
+                  path='/tesla_model_s/scene.gltf'
+                  scale={new Array(3).fill(0.013)}
+                // position={[-4, 0.2, 0]}
+                />
+              </BoundingBox>
             </Dragable>
           </Suspense>
           <Suspense fallback={null}>
